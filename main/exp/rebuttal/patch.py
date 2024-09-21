149b55e3569c3a8a0e86c790e50094a8e56e35b3
Konokiii <zd662@nyu.edu>
2024-09-21 09:48:54 +0800

    fix failed runs

--- main/exp/rebuttal/patch.py
+++ main/exp/rebuttal/patch.py
@@ -19,33 +19,31 @@ def main():
     args = parser.parse_args()
 
     settings = [
-        {'env': 'reacher',
-         'data_size': 1000,
-         'arch': '256-R-256-R-256-R|T',
-         'max_epochs': int(1.2e6),
-         'lamW': 0,
-         'seed': 1},
-
-        {'env': 'reacher',
-         'data_size': 1000,
-         'arch': '256-R-256-R-256-R|T',
-         'max_epochs': int(2e6),
-         'lamW': 0,
-         'seed': 0},
-
-        {'env': 'swimmer',
-         'data_size': 1000,
-         'arch': '256-R-256-R-256-R|T',
-         'max_epochs': int(2e6),
-         'lamW': 0,
-         'seed': 0},
+        {'env': 'hopper',
+         'mode': 'bn_no_relu',
+         'max_epochs': int(1e4),
+         'data_size': int(1e5),
+         'arch': '256-BR-256-BR-256|T',
+         'lamH': 1e-4,
+         'lamW': 1e-4,
+         'lr': 1e-2,
+         'eval_freq': 10,
+         'seed': 2,
+         'num_eval_batch': 400,
+         'group': 'bn_case2'},
 
         {'env': 'hopper',
-         'data_size': 10000,
-         'arch': '256-R-256-R-256-R|T',
-         'max_epochs': int(4e5),
-         'lamW': 0,
-         'seed': 0}
+         'mode': 'no_relu',
+         'max_epochs': int(2e5),
+         'data_size': int(1e4),
+         'arch': '256-R-256-R-256|T',
+         'lamH': -1,
+         'lamW': 5e-5,
+         'lr': 1e-2,
+         'eval_freq': 100,
+         'seed': 1,
+         'num_eval_batch': 100,
+         'group': 'no_bn'}
     ]
 
     actual_setting = settings[args.setting]
@@ -53,15 +51,17 @@ def main():
     """replace values & global setup"""
     config = TrainConfig(**actual_setting)
     config.device = DEVICE
-    config.lamH = -1
-    config.lr = 1e-2
-
-    config.eval_freq = 100
 
     config.data_folder = '/NC_regression/dataset/mujoco'
-    config.project = 'NC_rebuttal'
-    config.group = 'first'
-    hyper2logname = {
+    config.project = 'NC_new'
+    hyper2logname1 = {
+        'env': 'E',
+        'max_epochs': 'Eps',
+        'data_size': 'DS',
+        'lamW': 'W',
+        'seed': 's',
+    }
+    hyper2logname2 = {
         'env': 'E',
         'mode': 'M',
         'max_epochs': 'Eps',
@@ -69,6 +69,8 @@ def main():
         'lamW': 'wd',
         'seed': 's',
     }
+
+    hyper2logname = hyper2logname1 if config.lamH != -1 else hyper2logname2
     config.name = '_'.join([v + str(getattr(config, k)) for k, v in hyper2logname.items() if v != ''])
 
     run_BC(config)
